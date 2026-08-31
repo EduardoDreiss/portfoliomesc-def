477070f5669243896b283c2a8a270da741020aeb
EduardoDreiss <reisedardo@gmail.com>
2026-08-31 12:03:17 -0300

    commit inicial

---
+++ porfolioMesc/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <nav className='navbar'>
+        <div>
+          <p>Mesc</p>
+        </div>
+        <ul>
+          <li><a href="#">Meus trabalhos</a></li>
+        </ul>
+      </nav>
+      <section className='hero'>
+
+      </section>
+      <section className='little-presentation'>
+        <p>@duda.msc</p>
+      </section>
+    </>
+  )
+}
+
+export default App
