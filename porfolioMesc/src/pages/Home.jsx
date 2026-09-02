e50ea388654cdbb2731ea8dabfd59c19e5f320e9
EduardoDreiss <reisedardo@gmail.com>
2026-09-02 19:41:44 -0300

    implementação do router dom no app.jsx e criação do home.jsx importado pelo app.jsx

---
+++ porfolioMesc/src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import '../styles/home.css';
+import Navbar from '../components/Navbar';
+import Button from '../components/Button';
+import Carroussel from '../components/Carroussel';
+import Badge from '../components/Badge';
+
+function Home() {
+
+  return (
+    <>
+      {/* <Navbar/> */}
+      <section className='hero'>
+
+        <div className='firstBadge'>
+            <Badge text={'ME CONHEÇA'}/>
+        </div>
+
+      </section>
+      <section className='little-presentation'>
+        <div className='container'>
+        <h1 className='bigTextPresentation'>HELLO, I'M</h1>
+        <h1 className='bigTextPresentation'>MARIA</h1>
+        <h1 className='bigTextPresentation'>EDUARDA</h1>
+        <h1 className='bigTextPresentation'><strong>S</strong>ANTANA</h1>
+        </div>
+        <h2 className='smalTextPresentation'>and welcome to my portfolio.</h2>
+        <div className='mary'></div>
+      </section>
+      
+      <section className='carrousel'>
+        <h1 className='qualquer'>Algumas de minhas obras:</h1>
+        <Carroussel/>
+
+        <div className='buttonCarousel'>
+
+        <Button
+        text={"Conheça minhas obras aqui."}
+        link={"/obras"}
+        />
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Home;
