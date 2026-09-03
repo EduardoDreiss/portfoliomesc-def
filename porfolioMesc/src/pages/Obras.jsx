4bf1ce3143f3a111d3e601b435025de86d9e1946
EduardoDreiss <reisedardo@gmail.com>
2026-09-03 16:45:26 -0300

    --

--- porfolioMesc/src/pages/Obras.jsx
+++ porfolioMesc/src/pages/Obras.jsx
@@ -1,5 +1,18 @@
+import Badge from "../components/Badge"
+import Navbar from "../components/Navbar"
+
+import '../styles/obras.css'
+
+
 export default function Obras(){
     return(
-        <p>passou</p>
+        <>
+        <Navbar/>
+        <main className="mainObras"> 
+            
+        </main>
+        
+        </>
+ 
     )
 }
